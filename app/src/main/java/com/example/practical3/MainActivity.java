261bd2066751d455478aae8201dee64a87c25643
Tinotenda Mhedziso <tinomhedziso22@gmail.com>
2025-03-10 13:49:51 +0200

    Part 2 Added

--- app/src/main/java/com/example/practical3/MainActivity.java
+++ app/src/main/java/com/example/practical3/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.practical3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,7 +17,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity implements AdapterView. OnItemSelectedListener {
+    double pricePerTicket = 79.99;
+    int totalNumberOfTickets;
+    double totalPrice;
+    String itemChoice;
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String myConcertText = parent.getItemAtPosition(position).toString();
@@ -40,5 +50,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView. OnIt
         myConcertAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myConcertSpinner.setAdapter(myConcertAdapter);
         myConcertSpinner.setOnItemSelectedListener(this);
+
+
+        final EditText numberOfTickets = (EditText) findViewById(R.id.txtNumberOfTickets);
+
+        Button calculateTotalCost = (Button) findViewById(R.id.btnCost);
+
+        calculateTotalCost.setOnClickListener(new View.OnClickListener() {
+            final TextView totalCost = ((TextView) findViewById(R.id.txtTotalCostDisplay));
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                totalNumberOfTickets = Integer.parseInt(numberOfTickets.getText().toString());
+                totalPrice = totalNumberOfTickets * pricePerTicket;
+                DecimalFormat currency = new DecimalFormat("R###,###.##");
+                itemChoice = myConcertSpinner.getSelectedItem().toString();
+                totalCost.setText("The total cost for the " + itemChoice + " concert tickets is " + currency.format(totalPrice));
+            }
+        });
     }
 }
